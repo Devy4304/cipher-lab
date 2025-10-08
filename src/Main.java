369e42c1053517a84b5002530bf9a11c3ce2b076
Devy4304 <148592924+Devy4304@users.noreply.github.com>
2025-10-07 17:37:46 -0700

    Refactor Vigenere cipher key processing logic and adjust example usage in Main.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(VigenereCipher.encryptVigenere("This is a secret message!!!", "tacos", true));
+        System.out.println(VigenereCipher.encryptVigenere("The essence of life is but a mystery, unknown.", "abcdefghijklmnop", true));
     }
 }
--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -4,7 +4,7 @@ public class VigenereCipher {
     public static String encryptVigenere(String original, String key, boolean keepUppercase) {
         if (original.isEmpty() || key.isEmpty()) return "Invalid String or Key";
 
-        key = processKey(key, original.length());
+        key = processKey(key, original);
 
         System.out.println(original);
         System.out.println(key);
@@ -14,41 +14,30 @@ public class VigenereCipher {
         char[] keyArray = key.toCharArray();
 
         int letterIndex;
-        int textIndex = 0;
+        int keyIndex = 0;
 
         for (char letter : originalArray) {
             if (String.valueOf(letter).matches("^[A-Za-z]$")) {
                 letterIndex = getIndexInAlphabet(letter);
-                letterIndex += getIndexInAlphabet(keyArray[textIndex]);
+                letterIndex += getIndexInAlphabet(keyArray[keyIndex]);
 
                 if (letterIndex < 0) letterIndex += 26;
 
                 char character = alphabet[letterIndex % alphabet.length];
                 out.append((Character.isLowerCase(letter) && keepUppercase) ? character : Character.toUpperCase(character));
+
+                keyIndex++;
             } else {
                 out.append(letter);
             }
-
-            textIndex++;
         }
         return String.valueOf(out);
     }
 
     private static String processKey(String key, String original) {
         int numberOfKeyRepeats = (original.length() / key.length()) + 1;
-        StringBuilder keyOut = new StringBuilder(key.repeat(numberOfKeyRepeats));
-
-        int index = original.indexOf(" ");
-        if (index > 0) {
-            keyOut.insert(index, " ");
-        }
-        while(index >= 0) {
-            System.out.println(index);
-            index = original.indexOf(" ", index+1);
-            keyOut.insert(index, " ");
-        }
-
-        return String.valueOf(keyOut.substring(0, original.length()));
+        key = key.repeat(numberOfKeyRepeats);
+        return key.substring(0, original.length());
     }
 
     private static int getIndexInAlphabet(char letter) {
