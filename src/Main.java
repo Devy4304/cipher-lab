2d36876049a59eda776c5b5c59d10eda5ec9db6c
Devin <devin.lisiecki@icloud.com>
2025-10-09 12:06:40 -0700

    Add tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(VigenereCipher.encryptVigenere("This is a secret message!!!", "tacos", true, false));
+        System.out.println("Running tests...");
+
+        System.out.println("Testing Vigenere Cipher:");
+        System.out.println(("Mhkg al a usukev awlscuw!!!".equals(VigenereCipher.encryptVigenere("This is a secret message!!!", "tacos", true, false))) ? "Test passed" : "Test failed");
+        System.out.println(("G Vqdih, Jgqgf, aawjihfvx sjx vysn Jgqgf?".equals(VigenereCipher.encryptVigenere("O Romeo, Romeo, wherefore art thou Romeo?", "secrets", true, false))) ? "Test passed" : "Test failed");
+        System.out.println(("Tig hwxkukn yq xvtt it dxx f sfacock, hbznpyq.".equals(VigenereCipher.encryptVigenere("Tig hwxkukn yq xvtt it dxx f sfacock, hbznpyq.", "abcdefghijklmnop", true, false))) ? "Test passed" : "Test failed");
     }
 }
