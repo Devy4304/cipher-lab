a0a6248c083e33d0e16240551b1d96a31323889e
Devin <devin.lisiecki@icloud.com>
2025-10-10 11:55:23 -0700

    Fix issue where one of tests is comparing the wrong thing........

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Testing Vigenere Cipher:");
         System.out.println(("Mhkg al a usukev awlscuw!!!".equals(VigenereCipher.encryptVigenere("This is a secret message!!!", "tacos", true, false))) ? "Test passed" : "Test failed");
         System.out.println(("G Vqdih, Jgqgf, aawjihfvx sjx vysn Jgqgf?".equals(VigenereCipher.encryptVigenere("O Romeo, Romeo, wherefore art thou Romeo?", "secrets", true, false))) ? "Test passed" : "Test failed");
-        System.out.println(("Tig hwxkukn yq xvtt it dxx f sfacock, hbznpyq.".equals(VigenereCipher.encryptVigenere("Tig hwxkukn yq xvtt it dxx f sfacock, hbznpyq.", "abcdefghijklmnop", true, false))) ? "Test passed" : "Test failed");
+        System.out.println(("Tig hwxkukn yq xvtt it dxx f sfacock, hbznpyq.".equals(VigenereCipher.encryptVigenere("The essence of life is but a mystery, unknown.", "abcdefghijklmnop", true, false))) ? "Test passed" : "Test failed");
         System.out.println(" ");
         System.out.println("Testing Bifid Cipher:");
         System.out.println(("yfdncsatyekbfvt".equals(BifidCipher.encryptBifid("bifid cipher test", "key")) ? "Test passed" : "Test failed"));
